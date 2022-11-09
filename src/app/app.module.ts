d1009a882d42ad7de13cef7f80ee8566ce085fd1
Mykhaylik Alexander <mykhaylik@gmail.com>
2022-11-09 12:41:50 +0200

    much better than React

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,9 +14,12 @@ import { ArticleComponent } from './home/article/article.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
--- src/app/home/article/article.component.ts
+++ src/app/home/article/article.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-
+  articleName = "This is name of the article"
+  @Input() someValue: any;
+  number1 = '1'
+  pressed() {
+    document.createElement('p').innerHTML = this.number1
+  }
   constructor() { }
 
   ngOnInit(): void {
